32f554765f45754aca8a68c74c9191c989c678ed
Uthoai Marma <uthoaimarma597@gmail.com>
2024-02-12 00:28:07 +0600

    initial commit

--- Java01Hello.java
+++ Java01Hello.java
@@ -1,6 +1,14 @@
 public class Java01Hello {
     public static void main(String[] args){
-        System.out.println("Hello World...");
         System.out.println("Hello Java...");
+
+        String firstName = "Uthoai";
+        String lastName = "Marma";
+        System.out.println("Name: " + firstName + " " + lastName);
+
+        int id = 1054;
+        double gpa = 3.22;
+        System.out.println("ID: " + id);
+        System.out.println("GPA: " + gpa);
     }
 }
