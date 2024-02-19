c2bdf69370aebf50e0e1226d88a43763d708c20a
Uthoai Marma <uthoaimarma597@gmail.com>
2024-02-20 01:06:00 +0600

    insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,28 @@
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i-1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+    }
+
+    public static void printArray(int[] arr){
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {7,8,3,2,1};
+
+        insertionSort(arr);
+        printArray(arr);
+    }
+    
+}
